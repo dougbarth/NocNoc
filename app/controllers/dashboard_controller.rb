9754741083c7ef4c8a743d9a744a1e1d25014fc7
Doug Barth <dougbarth@gmail.com>
2010-03-19 10:42:08 -0500

    Making dashboard the root URL.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,14 +9,4 @@ class DashboardController < ApplicationController
     @current_panel = Panel.find(params[:current_id])
     @next_panel = @current_panel.last? ? Panel.first : @current_panel.lower_item
   end
-
-  def load_page
-    render :text => params[:page]
-  end
-
-  private
-  def panel_urls
-    [['Page 1', url_for(:action => :load_page, :page => 'page1.html')],
-      ['Page 2', url_for(:action => :load_page, :page => 'page2.html')]]
-  end
 end
